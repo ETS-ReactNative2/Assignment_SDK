e243c248f6ff00271e4b590af348d81d2e7b1c95
daksh-innovaccer <daksh.bindal@innovaccer.com>
2022-04-23 00:46:09 +0530

    assignment

---
+++ node/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const helmet = require("helmet") // to protect from mal-practioners (attacks)
+const cors = require("cors")
+
+app.use(express.json()) //middleware
+app.use(express.urlencoded({ extended: true })) //middleware
+
+app.use(helmet())
+app.use(cors())
+
+app.use("/s3", require("./routes/routes"))
+
+app.get("/", (req, res) => { // root path
+    res.send("<h1>Welcome to User CRUD</h1>")
+})
+
+// app.get('/list', async(req,res)=>{
+//     let data;
+//     list(function(fn){
+//         data=fn
+//     })
+//     // data.forEach(element => {
+//     //     name = element.Name;
+//     // });
+//     res.send(data)
+//     // console.log(data)
+// })
+
+app.listen(3001, () => {
+    console.log("Server Started")
+})
